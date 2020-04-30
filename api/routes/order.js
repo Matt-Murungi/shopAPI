2c1b0fb08540c1ee16776857da6fb771fbe6b510
Matt-Murungi <murungima@gmail.com>
2020-04-30 13:51:51 +0300

    added order route

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,34 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-    res.status(200).json({
-        message: "Order has been gotten"
-    });
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Order has been gotten",
+  });
 });
 
-router.post('/', (req, res, next)=>{
-    res.status(201).json({
-        message: "Order has been created"
-    });
+router.post("/", (req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity,
+  };
+  res.status(201).json({
+    message: "Order has been created",
+    order: order,
+  });
 });
 
-router.get('/:id', (req, res, next)=>{
-    const id = req.params.id;
+router.get("/:id", (req, res, next) => {
+  const id = req.params.id;
 
-   res.status(200).json({
-       message : "Order has been gotten",
-       orderId: id
-   })
-})
+  res.status(200).json({
+    message: "Order has been gotten",
+    orderId: id,
+  });
+});
 
-router.delete('/:id', (req, res, next)=>{
-    const id = req.params.id;
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
 
-   res.status(200).json({
-       message :  "Order has been deleted",
-       orderId: id
-   })
-})
+  res.status(200).json({
+    message: "Order has been deleted",
+    orderId: id,
+  });
+});
 
-module.exports = router; 
+module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/order');
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/order");
 
+app.use(morgan("dev"));
 
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.header("Access-Control-Allow-Origin", "*");
+  req.header("Access-Control-Allow-Headers", "*");
+
+  if (req.method === "OPTIONS") {
+    req.header("Access-Control-Allow-Methods", "*");
+    res.send(200).json({});
+  }
+  next();
+});
+
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
